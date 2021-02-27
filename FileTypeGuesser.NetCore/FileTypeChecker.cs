8ade6fc2011d6b657f21616b33ee6fb446a57cf0
Danel Sibaev <danon.sibaev@gmail.com>
2021-02-27 14:26:16 +0300

    more formats

--- FileTypeGuesser.NetCore/FileTypeChecker.cs
+++ FileTypeGuesser.NetCore/FileTypeChecker.cs
@@ -14,14 +14,20 @@ namespace FileTypeGuesser.NetCore
             new("Graphics Interchange Format 87a", ".gif", new ExactFileTypeMatcher(new byte[] {0x47, 0x49, 0x46, 0x38, 0x37, 0x61})),
             new("Graphics Interchange Format 89a", ".gif", new ExactFileTypeMatcher(new byte[] {0x47, 0x49, 0x46, 0x38, 0x39, 0x61})),
             new ("Text", ".tiff", new ExactFileTypeMatcher(new byte[] {0x49, 0x20, 0x49})),
-            // new ("Text", ".pcx", new ExactFileTypeMatcher()),
+            // new ("Text", ".pcx", new ExactFileTypeMatcher(new byte[] {})),
             
-            // new ("Zip archive", ".zip", new ExactFileTypeMatcher()),
-            // new ("Rar archive", ".rar", new ExactFileTypeMatcher()),
-            // new ("7z archive", ".7z", new ExactFileTypeMatcher()),
+            new ("Zip archive", ".zip", new ExactFileTypeMatcher(new byte[] {	0x50, 0x4B, 0x03, 0x04})),
+            new ("Zip archive", ".zip", new ExactFileTypeMatcher(new byte[] {	0x50, 0x4B, 0x05, 0x06})),
+            new ("Zip archive", ".zip", new ExactFileTypeMatcher(new byte[] {	0x50, 0x4B, 0x07, 0x08})),
+            new ("Zip archive", ".zip", new ExactFileTypeMatcher(new byte[] {	0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x01, 0x00})),
+            new ("Zip archive", ".zip", new ExactFileTypeMatcher(new byte[] {	0x63, 0x00, 0x00, 0x00, 0x00, 0x00,})),
+            new ("Zip archive", ".zip", new ExactFileTypeMatcher(new byte[] {	})),
+            new ("Rar archive", ".rar", new ExactFileTypeMatcher(new byte[] {	0x52, 0x61, 0x72, 0x21, 0x1A, 0x07, 0x01, 0x00})),
+            new ("Rar archive", ".rar", new ExactFileTypeMatcher(new byte[] {	0x52, 0x61, 0x72, 0x21, 0x1A, 0x07, 0x00})),
+            new ("7z archive", ".7z", new ExactFileTypeMatcher(new byte[] {0x37, 0x7A, 0xBC, 0xAF, 0x27, 0x1C})),
             
-            // new ("Text", ".txt", new ExactFileTypeMatcher()),
-            // new ("Text", ".rtf", new ExactFileTypeMatcher()),
+            // new ("Text", ".txt", new ExactFileTypeMatcher(new byte[] {})),
+            new ("Text", ".rtf", new ExactFileTypeMatcher(new byte[] {0x7B, 0x5C, 0x72, 0x74, 0x66, 0x31})),
 
             new("Portable Document Format", ".pdf", new RangeFileTypeMatcher(new ExactFileTypeMatcher(new byte[] {0x25, 0x50, 0x44, 0x46}), 1019)),
             
